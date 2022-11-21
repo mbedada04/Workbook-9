64e71cb667b25586f3e934de0d05666f91a682a0
mbedada04 <mbedada@atl.yearup.org>
2022-11-21 15:01:46 -0500

    done

--- exercise/extendsClass.js
+++ exercise/extendsClass.js
@@ -8,7 +8,17 @@ class Person {
   }
 }
 
-let person = new Person ( "Millena", "Bedada");
+let person = new Person("Millena", "Bedada");
 console.log(person.getFullName());
 
- 
+class Employee extends Person {
+  constructor(firstName, lastName, id, jobTitle, payRate) {
+    super(firstName, lastName);
+    this.id = id;
+    this.jobTitle = jobTitle;
+    this.payRate = payRate;
+  }
+}
+
+let employee = new Employee("Programmer", "Millena", 2, "App Dev", 2000000);
+console.log(employee.getFullName());
